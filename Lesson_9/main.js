54562f3c2160ab984685ba4a18fe031ef2443525
TaniaTV <tania.chizhik.55@gmail.com>
2022-05-25 20:01:14 +0300

    Homework_9

--- Lesson_9/main.js
+++ Lesson_9/main.js
@@ -1,12 +1,12 @@
 // #1 Написать функцию, принимающую массив имен и возвращающую массив объектов вида {name: 'Vasya'}.
 arr = ['ann', 'kate', 'mari', 'pit',]
 function createObj(arr) {
-  var newArr = [];
-  arr.map(function (i) {
-    arr[i] = { name: i };
-    newArr.push(arr[i]);
-  });
-  return newArr;
+    var newArr = [];
+    arr.map(function (i) {
+        arr[i] = { name: i };
+        newArr.push(arr[i]);
+    });
+    return newArr;
 };
 createObj(arr);
 
@@ -15,24 +15,30 @@ createObj(arr);
 */
 var arr = ['00', '13', '24'];
 function makeTime(arr) {
-  var time;
-  arr.reduce(function (index, num) {
-    time += ':' + num;
-  }); return '"Текущее время :' + time;
+    var time;
+    time = arr.join(':');
+    return '"Текущее время : ' + time + '"';
 }
 alert(makeTime(arr));
-
 
 /*Задание 3:
 Написать функцию, которая будет возвращать количество гласных в переданном тексте. Регистр любой. Решение не
 должно быть "топорным".
 */
+
 var str = 'напрямую свяжут газовые месторождения';
 function getVowels(str) {
-  newArr = str.toLowerCase().split('').filter (function (i) {
-    return 'аоуыэеёиюя'.includes(i)}).length;
-    return 'Количество гласных = ' + newArr;
+    var letters = 'аоуыэеёиюя';
+    newArr = str.toLowerCase().split('');
+    var lengthVolves = 0;
+
+    for (var i = 0; i < newArr.length; i++) {
+        if (letters.indexOf(newArr[i]) !== -1) {
+            lengthVolves++;
+        };
+    }; return 'Количество гласных = ' + lengthVolves;
 };
+getVowels(str);
 console.log(getVowels(str));
 
 
@@ -52,17 +58,19 @@ console.log(getVowels(str));
 
 var str = 'Привет, студент! Студент... Как дела, студент?';
 function countSentencesLetters(str) {
-  var newStr = str.split(/[.!?]/);
-  for (let i = 0; i <= newStr.length
-    - 1; i++) {
-    var string = newStr[i]; // это разбивает по сторокам Н: Привет, студент
-
-    var lengthStr = newStr[i].split(/[' ', ',']/);// это должно разбить на слова Н: [Привет, студент,]
-
-    var result = lengthStr.reduce(function (sum, elem, i, arr) { // это должно посчитать буквы в словах
-      return sum + lengthStr[i].length;
-    }, 0);
-
-  }; return console.log(string + ': Letters quantity is:' + result);
+    var arr = str.split(/[.!?]/);
+    for (var i = 0; i <= arr.length - 1; i++) {
+        var sentence = arr[i];
+        if (sentence != '') {
+            var words = sentence.split(/['',','' ']/);
+            var letters = 0;
+            for (var j = 0; j <= words.length - 1; j++) {
+                word = words[j];
+                letters += word.length
+            };
+            console.log(sentence + ': Количество букв:' + letters);
+        };
+    };
 };
 countSentencesLetters(str);
+
